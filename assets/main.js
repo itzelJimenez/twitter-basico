3764e0f15e01aa1cb458c0c816ec558fa1ea7f72
Itzel <itzeljimenez.do@gmail.com>
2017-04-03 17:58:15 +0000

    eventos funcionando

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,35 @@
 //alert("hola")
 var counter = 0;
+var button = document.getElementById("enviar");
+var textarea = document.getElementById("textarea");
+var perritos = document.getElementById("name");
+var print = document.getElementById("print");
+function counting(){
+	counter +=1;
+	var printables = document.getElementById("cntClicks");
+	printables.innerText = "Contador de clicks: " + counter;
+	}
 
-function counter(){
-	alert("Hola Juana");
+function submit(e) {
+	e.preventDefault();
+	alert("funciona chidoliro")
 }
 
+function noCounting(){
+	event.stopPropagation();
+}
+
+function charCount(){
+	var numChar = textarea.value.length;
+	print.innerText = numChar;
+
+}
+
+//funcion borrar, obtener codigo ascci de borrar, y disminuir el contador.
 //Eventos
-document.addEventListener("click", counter)
+document.addEventListener("click", counting);
+button.addEventListener("click", submit);
+button.addEventListener("click", noCounting);
+textarea.addEventListener("click", noCounting);
+perritos.addEventListener("click", noCounting);
+textarea.addEventListener("keypress", charCount)
