f392a8d5266d23c5a9773993936fc15c97cf6901
Itzel <itzeljimenez.do@gmail.com>
2017-05-31 14:27:50 +0000

    contador de likes y distribución correcta del dom

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ var cargarPagina=function(){
 	var $button = $("#enviar");
 	var $textarea = $("#textarea");
 	//Eventos
-	$(document).click(counting);
+	$(document).on("click", ".count", counting);
 	$button.click(function(e){
 		e.preventDefault();
 	});
@@ -16,33 +16,57 @@ var cargarPagina=function(){
 	$textarea.click(noCounting);
 	$nam.click(noCounting);
 	$textarea.click(charCount);
+	$(".button-collapse").sideNav();
 }
 
 function counting(){
 	counter +=1;
-	var $printables = $("#cntClicks");
+	var $printables = $(".count").find("span");
 	var printablesNode = counter;
-	$printables.append(printablesNode + ",");
+	$printables.text(printablesNode);
 }
 
 function send() {
 	if(textarea.value != " " || $nam.value != " "){
 		var $coments = $('#newComents');
-		var container =  $('<article/>', { 'class': 'card-panel hoverable blue lighten-4' });
-		var text = $('<p/>');
-		var txtNode = textarea.value;
-		var nameNode = "Por: " + $nam.value;
-		text.append(txtNode);
-		$coments.append(container);
+		var $container =  $('<article/>', { 'class': 'card-panel hoverable blue lighten-4' });
+		var $row1 =$('<div/>', {'class':'row'});
+		var $row2 =$('<div/>', {'class': 'row'});
+		var $textRow=$('<p/>');
+		var $textComent = $('<p/>');
+		var txtNode = " " +textarea.value;
+
+		var $iconUser =  $('<i/>', { 'class': 'fa fa-user-circle  fa-2x grey-text' });
+		var $nickName =$('<strong>	@Susanito : </strong>', {'class': 'grey-text'})
+		$coments.append($container);
 		//Botón eliminar
-		var remove = $('<a/>', { 'class': 'waves-effect waves-light blue btn right' });
-		var buttonNode = "x"
-		remove.append(buttonNode);
-		remove.click(function(){
-			remove.parent().remove();
+		var $smile= $('<a/>', { 'class': 'waves-effect  right' });
+		var $iconSmile = $('<i/>', { 'class': 'fa fa-smile-o  fa-2x orange-text' });
+		$smile.append($iconSmile);
+
+		var $like = $('<a/>', { 'class': 'waves-effect  right count' });
+		var $countLikes=$('<span/>',{'class': ''});
+		var $iconLike = $('<i/>', { 'class': 'fa fa-heart  fa-2x red-text' });
+		$like.append($iconLike);
+		$like.append($countLikes);
+
+		var $remove = $('<a/> ', { 'class': 'waves-effect right' });
+		var buttonNode = $('<i/>', { 'class': 'fa fa-times  fa-2x' });;
+		$remove.append(buttonNode);
+		$remove.click(function(){
+			$remove.parent().parent().parent().remove();
 		});
-		container.append(remove);
-		container.append(text);
+
+		$container.append($row1);
+		$container.append($row2);
+		$row1.append($textRow);
+		$textRow.append($iconUser);
+		$textRow.append($nickName);
+		$textRow.append($remove);
+		$textRow.append($like);
+		$textRow.append($smile);
+		$row2.append($textComent);
+		$textComent.append(txtNode);
 	} else {
 		alert("Por favor ingresa algo antes de enviar")
 	}
