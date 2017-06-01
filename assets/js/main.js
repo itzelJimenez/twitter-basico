c54b4140588f2cdff593ed9e3dee7416b1c28533
Itzel <itzeljimenez.do@gmail.com>
2017-06-01 17:32:50 +0000

    diseño responsivo, adición hora, botones flotantes

--- assets/js/main.js
+++ assets/js/main.js
@@ -55,43 +55,81 @@ function send() {
 		var $nickName =$('<a/>');
 		var name = "@ixpyMoonlight:";
 		$nickName.append(name);
-		$coments.append($container);
 		//Contenedor elementos usuario
-		var $elementsUser = $('<span/>',{'class': 'valign-wrapper col s6'});
+		var $elementsUser = $('<span/>',{'class': 'valign-wrapper col s6 m8 l9'});
 		$elementsUser.append($iconUser);
 		$elementsUser.append($nickName);
+		//Agregando fecha de publicación
+		var $spanTime = $('<span/>',{'class': 'valign-wrapper col s1 hide-on-small-only hide-on-down'});
+		var $time = getTime();
+		$spanTime.append($time);
 		//Botón emoticon
-		var $smile= $('<a/>', { 'class': 'waves-effect  right' });
+		var $emoticos = $('<span/>', {'class': 'fixed-action-btn horizontal right'});
+		var $smile= $('<a/>', { 'class': 'waves-effect '});
 		var $iconSmile = $('<i/>', { 'class': 'fa fa-smile-o  fa-2x orange-text' });
 		$smile.append($iconSmile);
+		$emoticos.append($smile);
+		//Anidación UL
+		var $ul =$('<ul/>');
+			//Anidación LI's
+			//Li 1
+		var $emoticon1 = $('<li/>');
+		var $contEmotic1 = $ ('<a/>',{'class': 'btn-floating'});
+		var $iconEmotic1 = $ ('<i/>',{'class': 'fa fa-smile-o fa-2x orange-text'});
+		$contEmotic1.append($iconEmotic1);
+		$emoticon1.append($contEmotic1);
+		$ul.append($emoticon1);
+			//Li 2
+		var $emoticon2 = $('<li/>');
+		var $contEmotic2 = $ ('<a/>',{'class': 'btn-floating'});
+		var $iconEmotic2 = $ ('<i/>',{'class': 'fa fa-frown-o fa-2x blue-text'});
+		$contEmotic2.append($iconEmotic2);
+		$emoticon2.append($contEmotic2);
+		$ul.append($emoticon2);
+			//LI 3
+		var $emoticon3 = $('<li/>');
+		var $contEmotic3 = $ ('<a/>',{'class': 'btn-floating'});
+		var $iconEmotic3 = $ ('<i/>',{'class': 'fa fa-meh-o fa-2x black-text'});
+		$contEmotic3.append($iconEmotic3);
+		$emoticon3.append($contEmotic3);
+		$ul.append($emoticon3);
+		//anidación UL
+		$emoticos.append($ul);
+
 		//botón like
-		var $like = $('<a/>', { 'class': 'waves-effect  right count' });
+		var $likes = $('<span/>', {'class': 'fixed-action horizontal right'});
+		var $like = $('<a/>', { 'class': 'waves-effect count' });
 		var $countLikes=$('<span/>',{'class': ''});
 		var $iconLike = $('<i/>', { 'class': 'fa fa-heart  fa-2x red-text' });
 		$like.append($iconLike);
 		$like.append($countLikes);
+		$likes.append($like);
 		//botón eliminar
-		var $remove = $('<a/> ', { 'class': 'waves-effect right' });
+		var $delete = $('<span/>', {'class': 'fixed-action horizontal right'});
+		var $remove = $('<a/> ', { 'class': 'waves-effect' });
 		var buttonNode = $('<i/>', { 'class': 'fa fa-times  fa-2x' });;
 		$remove.append(buttonNode);
 		$remove.click(function(){
-			$remove.parent().parent().parent().parent().remove();
+			$remove.parent().parent().parent().parent().parent().remove();
 		});
+		$delete.append($remove);
 		//contenedor botones 
-		var $elementsButtons = $('<span>');
-		$elementsButtons.append($remove);
-		$elementsButtons.append($like);
-		$elementsButtons.append($smile);
+		var $elementsButtons = $('<span/>',{'class': 'col s2'});
+		$elementsButtons.append($delete);
+		$elementsButtons.append($likes);
+		$elementsButtons.append($emoticos);
 		//Anidación de elementos
+		$coments.prepend($container);
 		$container.append($row1);
 		$container.append($row2);
 		$row1.append($textRow);
 		$textRow.append($elementsUser);
+		$textRow.append($spanTime)
 		$textRow.append($elementsButtons);
 		$row2.append($textComent);
 		$textComent.append(txtNode);
 	} else {
-		alert("Por favor ingresa algo antes de enviar")
+		alert("Por favor ingresa algo antes de enviar");
 	}
 }
 
@@ -109,6 +147,10 @@ function clear(){
 	textarea.value = " ";
 	$nam.value = " ";
 }
-
+function getTime(){
+	var now = new Date();
+	var time = now.getHours()+':'+ (now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes());
+	return time;
+}
 $(document).ready(cargarPagina);
 }) ();
